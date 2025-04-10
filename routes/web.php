f5f1f064bf0614ba08dedbedd6e4874035d164bf
Richmond-Nunoo <1957richmond@gmail.com>
2025-04-10 10:28:22 +0000

    update .evn example

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', [DriverController::class, 'index'])->name('index');
 // Form to register a new driver (same as index in this case)
 Route::post('/register-driver', [DriverController::class, 'store'])->name('drivers.store');
 
-// Form to check driver status
+// Form view to check driver status
 Route::get('/check-status', [DriverController::class, 'showCheckForm'])->name('drivers.check.form');
 
 // POST route to process check status form
@@ -19,6 +19,7 @@ Route::post('/check-status', [DriverController::class, 'checkStatus'])->name('dr
 
 // Driver dashboard route (after successful registration or lookup)
 Route::get('/driver-dashboard/{driver}', [DriverController::class, 'driversDashboard'])->name('drivers.dashboard');
+
 
 
 
